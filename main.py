e927051d6fa1c72eb05dc0b309d3256f3ae2fcb1
franmillan04 <franciscopablomillanortiz@gmail.com>
2026-04-19 13:01:25 +0200

    fix: Subject email problem

--- main.py
+++ main.py
@@ -2,11 +2,20 @@
 
 from src.client import fetch_emails
 
+def get_subject(email):
+    headers = email.get("payload", {}).get("headers", [])
+    
+    for h in headers:
+        if h["name"].lower() == "subject":
+            return h["value"]
+    
+    return "Sin asunto"
+
 def main():
     emails = fetch_emails()
     
     for email in emails:
-        print(email["subject"])
+        print(get_subject(email))
 
 if __name__ == "__main__":
     main()
